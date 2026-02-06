eb65bf50abab6c1101809a2d8e18b35c1cea1f8a
majorabrito <marialeirabrito@gmail.com>
2026-02-07 00:15:06 +0100

    Added API and functions for the temperature

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
+function displayTemperature(response){
+    let temperatureElement = document.querySelector("#temperature");
+let temperature = Math.round(response.data.main.temp);
+    temperatureElement.innerHTML = temperature;
+}
+
+
 function searchData(city){
     let apiKey="6b6a2d3686b7a15a7b03d3319d1627b9"
-    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+     axios.get(apiUrl).then(displayTemperature);
 }
 
 
@@ -10,6 +18,7 @@ function searchFormCity(event) {
 let city = document.querySelector("#city");
 city.innerHTML= searchElement.value; 
 
+searchData(searchElement.value);
 }
 
 let searchForm = document.querySelector("#search-form");
