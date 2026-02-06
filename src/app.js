6f7a1dffd04f5e409b4e98eaefdb3a90aff43a77
majorabrito <marialeirabrito@gmail.com>
2026-02-07 00:33:04 +0100

    Correction of smalls detailsin the searching form

--- src/app.js
+++ src/app.js
@@ -1,26 +1,23 @@
 function displayTemperature(response){
     let temperatureElement = document.querySelector("#temperature");
 let temperature = Math.round(response.data.main.temp);
+city.innerHTML= response.data.name;
     temperatureElement.innerHTML = temperature;
 }
-
-
 function searchData(city){
     let apiKey="6b6a2d3686b7a15a7b03d3319d1627b9"
     let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
      axios.get(apiUrl).then(displayTemperature);
 }
 
-
 function searchFormCity(event) {
     event.preventDefault();
     let searchElement = document.querySelector("#search-form-input");
 let city = document.querySelector("#city");
-city.innerHTML= searchElement.value; 
-
 searchData(searchElement.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchFormCity);
 
+searchFormCity("Malaga");
