b244a5459f8217cf3ea412488a2b2e007e4925c8
majorabrito <marialeirabrito@gmail.com>
2026-02-07 17:23:34 +0100

    Added humidity and wind with API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 function displayTemperature(response){
     let temperatureElement = document.querySelector("#temperature");
 let temperature = Math.round(response.data.main.temp);
+let humidityElement = document.querySelector("#Humidity");
+let humidity = response.data.main.humidity;
+let windElement = document.querySelector("#Wind");
+let wind = response.data.wind.speed;
+windElement.innerHTML = `${wind}km/h`;
+humidityElement.innerHTML= `${humidity}%`;
 city.innerHTML= response.data.name;
     temperatureElement.innerHTML = temperature;
+
+    console.log(wind);
 }
 function searchData(city){
     let apiKey="6b6a2d3686b7a15a7b03d3319d1627b9"
@@ -19,5 +27,3 @@ searchData(searchElement.value);
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchFormCity);
-
-searchFormCity("Malaga");
